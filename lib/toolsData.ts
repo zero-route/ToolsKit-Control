8731472d267d910177e23a31cc61e0e91b797179
zero-route <iostream911@gmail.com>
2026-09-19 23:49:06 +0800

    update toolsData.ts add new control tools  { id: 'dns-lookup', name: 'DNS Lookup' }, { id: 'ping-tester', name: 'Ping/Latency tester' }, { id: 'mac-vendor-lookup', name: 'MAC Address vendor lookup' }, { id: 'whois-lookup', name: 'WHOIS/domain info lookup' }

--- lib/toolsData.ts
+++ lib/toolsData.ts
@@ -57,6 +57,10 @@ export const categories: AdminCategory[] = [
       { id: 'ip-network-info', name: 'IP and network info' },
       { id: 'subnet-calculator', name: 'CIDR/subnet calculator' },
       { id: 'timestamp-converter', name: 'Epoch timestamp converter' }
+      { id: 'dns-lookup', name: 'DNS Lookup' },
+      { id: 'ping-tester', name: 'Ping/Latency tester' },
+      { id: 'mac-vendor-lookup', name: 'MAC Address vendor lookup' },
+      { id: 'whois-lookup', name: 'WHOIS/domain info lookup' }
     ]
   }
 ]
